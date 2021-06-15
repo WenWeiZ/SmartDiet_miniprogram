180e41da9a1948dc7d1679b6434724f0375f07d5
YizhePKU <yizhe@pku.edu.cn>
2021-06-15 19:09:04 +0800

    Fix clearStorage error

--- app.js
+++ app.js
@@ -1,29 +1,2 @@
-import { request, takePhoto, uploadFile } from 'utils/all';
-import { formatDate } from 'utils/time';
-
 App({
-  onLaunch: async function() {
-    let init_data = {
-      "9e03e9fe-1c52-4ab7-8433-7fbbe58707a5": {
-        id: "9e03e9fe-1c52-4ab7-8433-7fbbe58707a5",
-        img: "",
-        name: "示例菜品",
-        calories: 123,
-        weight_before: 43,
-        weight_after: 26,
-        date: formatDate(new Date())
-      },
-    };
-
-    try {
-      let data = await wx.getStorage({
-        key: "dishes",
-      });
-    } catch (err) {
-      await wx.setStorage({
-        key: "dishes",
-        data: init_data,
-      });
-    }
-  },
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,12 +1,34 @@
 import { request, sleep } from "../../utils/all";
+import { formatDate } from '../../utils/time';
 
 Page({
   data: {},
 
   getDishes: async function() {
-    return (await wx.getStorage({
-      key: "dishes",
-    })).data;
+    let init_data = {
+      "9e03e9fe-1c52-4ab7-8433-7fbbe58707a5": {
+        id: "9e03e9fe-1c52-4ab7-8433-7fbbe58707a5",
+        img: "",
+        name: "示例菜品",
+        calories: 123,
+        weight_before: 43,
+        weight_after: 26,
+        date: formatDate(new Date())
+      },
+    };
+
+    try {
+      let data = await wx.getStorage({
+        key: "dishes",
+      });
+      return data.data;
+    } catch (err) {
+      await wx.setStorage({
+        key: "dishes",
+        data: init_data,
+      });
+      return {};
+    }
   },
 
   refresh: async function() {
